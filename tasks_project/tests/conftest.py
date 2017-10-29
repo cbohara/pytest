322fe721354c89f3358444bb4bc56bdc85894bed
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-10-29 16:57:54 -0700

    build fixture for function scope

--- tasks_project/tests/conftest.py
+++ tasks_project/tests/conftest.py
@@ -1,3 +1,62 @@
-"""Placeholder."""
+import pytest
+import tasks
+from tasks import Task
 
-# nothing here yet
+
+@pytest.fixture()
+def tasks_db(tmpdir):
+    """Connect to db before test, disconnect after"""
+    # setup
+    # the value of tmpdir is an object that represents a directory
+    # use str to pass the filepath to the start_tasks_db function
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+    yield   # this is where the testing happens
+    # teardown is guaranteed to run regardless of what happens with the tests
+    tasks.stop_tasks_db()
+
+
+# Reminder of Task constructor interface
+# Task(summary=None, owner=None, done=False, id=None)
+# summary is required
+# owner and done are optional
+# id is set by database
+
+
+@pytest.fixture()
+def tasks_just_a_few():
+    """All summaries and owners are unique."""
+    return (
+        Task('Write some code', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False))
+
+
+@pytest.fixture()
+def tasks_mult_per_owner():
+    """Several owners with several tasks each."""
+    return (
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel'))
+
+
+@pytest.fixture()
+def db_with_3_tasks(tasks_db, tasks_just_a_few):
+    """Connect to db with 3 unique tasks"""
+    for t in tasks_just_a_few:
+        tasks.add(t)
+
+
+@pytest.fixture()
+def db_with_multiple_per_owner(tasks_db, tasks_mult_per_owner):
+    """Connect db with 9 tasks, 3 owners, all with 3 tasks"""
+    for t in tasks_mult_per_owner:
+        tasks.add(t)
--- tasks_project/tests/func/test_add.py
+++ tasks_project/tests/func/test_add.py
@@ -2,34 +2,21 @@ import pytest
 import tasks
 from tasks import Task
 
-@pytest.fixture(autouse=True)
-def initialized_task_db(tmpdir):
-    """Connect to db before testing and disconnect after."""
-    # Setup : start db
-    tasks.start_tasks_db(str(tmpdir), 'tiny')
-    yield # this is where the testing happens
-    # Teardown : stop db
-    tasks.stop_tasks_db()
 
-def test_add_returns_valid_id():
-    """tasks.add(<valid task>) should return an int."""
-    # GIVEN an initialized tasks db
+def test_add_returns_valid_id(tasks_db):
+    """tasks.add(<valid task>) should return an integer"""
+    # GIVEN an initialized db
     # WHEN a new task is added
-    # THEN returned task_id is of type int
     new_task = Task('do something')
+    # THEN returned task_id is of type int
     task_id = tasks.add(new_task)
     assert isinstance(task_id, int)
 
-@pytest.mark.smoke
-def test_added_task_has_id_set():
-    """Make sure that the task_id field is set by tasks.add()"""
-    # GIVEN an initialized tasks db
-    # AND a new task is added
-    new_task = Task('sit in chair', owner='me', done=True)
-    task_id = tasks.add(new_task)
 
-    # WHEN task is retrieved
-    task_from_db = tasks.get(task_id)
-
-    # THEN task_id matches id field
-    assert task_from_db.id == task_id
+def test_add_increases_count(db_with_3_tasks):
+    """Test tasks.add() affects task.count()"""
+    # GIVEN an initialized db with 3 tasks
+    # WHEN another task is added
+    tasks.add(Task('do something'))
+    # THEN the count increases by one
+    assert tasks.count() == 4
